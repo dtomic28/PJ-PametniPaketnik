24bc718ab61292bb7a61c4294db4dcebc044178e
Tilen Gašparič <tilen623@gmail.com>
2025-06-04 11:10:10 +0200

    added lazy column to main menu, gets data from backend

---
+++ app/src/main/java/com/dtomic/pametnipaketnik/composable/pages/Page_BuyItem.kt
@@ -0,0 +1,10 @@
+package com.dtomic.pametnipaketnik.composable.pages
+
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+
+@Composable
+fun Page_BuyItem(navController: NavController, viewModel: LoginViewModel = viewModel(), itemId: String) {
+
+}
