7922002da762fa14a3ddf58a9f56b63dd043552b
Tilen Gašparič <tilen623@gmail.com>
2025-06-07 13:31:37 +0200

    added profile and settings menus (templates for now)

--- app/src/main/java/com/dtomic/pametnipaketnik/composable/pages/Page_MainMenu.kt
+++ app/src/main/java/com/dtomic/pametnipaketnik/composable/pages/Page_MainMenu.kt
@@ -2,7 +2,12 @@ package com.dtomic.pametnipaketnik.composable.pages
 
 import android.util.Log
 import android.view.MenuItem
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,24 +18,30 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -57,7 +68,10 @@ import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
 import androidx.compose.ui.platform.LocalLifecycleOwner
-
+import com.dtomic.pametnipaketnik.composable.parts.Custom_UserDashboard
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.*
+import com.dtomic.pametnipaketnik.composable.parts.Custom_SettingsDashboard
 
 class MainMenuViewModel : ViewModel() {
     data class MenuItem(
@@ -135,6 +149,9 @@ class MainMenuViewModel : ViewModel() {
 
 @Composable
 fun Page_MainMenu(navController: NavController, viewModel: MainMenuViewModel = viewModel()) {
+    val showProfileMenu = remember { mutableStateOf(false) }
+    val showSettingsMenu = remember { mutableStateOf(false) }
+
     val errorMessage by viewModel.errorMessage.collectAsState()
     val error = errorMessage.isNotEmpty()
 
@@ -169,7 +186,7 @@ fun Page_MainMenu(navController: NavController, viewModel: MainMenuViewModel = v
         modifier = Modifier
             .fillMaxSize()
             .background(MaterialTheme.colorScheme.background),
-        contentAlignment = Alignment.Center
+        //contentAlignment = Alignment.Center
     ) {
         Column (
             modifier = Modifier
@@ -190,7 +207,7 @@ fun Page_MainMenu(navController: NavController, viewModel: MainMenuViewModel = v
                     contentAlignment = Alignment.CenterStart
                 ) {
                     IconButton(
-                        onClick = { /* Handle click */ },
+                        onClick = { showSettingsMenu.value = true },
                         modifier = Modifier
                             .background(Color.Transparent)
                             .fillMaxHeight()
@@ -211,7 +228,7 @@ fun Page_MainMenu(navController: NavController, viewModel: MainMenuViewModel = v
                     contentAlignment = Alignment.CenterEnd
                 ) {
                     IconButton(
-                        onClick = { /* Handle click */ },
+                        onClick = { showProfileMenu.value = true },
                         modifier = Modifier
                             .background(Color.Transparent)
                             .fillMaxHeight()
@@ -285,6 +302,61 @@ fun Page_MainMenu(navController: NavController, viewModel: MainMenuViewModel = v
                     text = stringResource(R.string.btn_sell),
                     onClick = { viewModel.moveToItemSell() },
                 )
+            }
+        }
+
+        if (showProfileMenu.value) {
+            Box(
+                Modifier
+                    .fillMaxSize()
+                    .background(Color.Black.copy(alpha = 0.3f))
+                    .blur(10.dp)
+                    .clickable { showProfileMenu.value = false }
+            )
+
+            // Slide-in from RIGHT
+            Box(modifier = Modifier.fillMaxSize()) {
+                AnimatedVisibility(
+                    visible = true,
+                    enter = slideInHorizontally(
+                        initialOffsetX = { it },
+                        animationSpec = tween(durationMillis = 300)
+                    ),
+                    exit = slideOutHorizontally(
+                        targetOffsetX = { it },
+                        animationSpec = tween(durationMillis = 300)
+                    ),
+                    modifier = Modifier.align(Alignment.CenterEnd)
+                ) {
+                    Custom_UserDashboard(onClose = { showProfileMenu.value = false })
+                }
+            }
+        }
+        if (showSettingsMenu.value) {
+            Box(
+                Modifier
+                    .fillMaxSize()
+                    .background(Color.Black.copy(alpha = 0.3f))
+                    .blur(10.dp)
+                    .clickable { showSettingsMenu.value = false }
+            )
+
+            // Slide-in from RIGHT
+            Box(modifier = Modifier.fillMaxSize()) {
+                AnimatedVisibility(
+                    visible = true,
+                    enter = slideInHorizontally(
+                        initialOffsetX = { it },
+                        animationSpec = tween(durationMillis = 300)
+                    ),
+                    exit = slideOutHorizontally(
+                        targetOffsetX = { it },
+                        animationSpec = tween(durationMillis = 300)
+                    ),
+                    modifier = Modifier.align(Alignment.CenterStart)
+                ) {
+                    Custom_SettingsDashboard(onClose = { showSettingsMenu.value = false })
+                }
             }
         }
     }
